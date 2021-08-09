52a631fd15fe350b9416d6450920cae25e986505
SaranyaChandrasekaran1 <88572359+SaranyaChandrasekaran1@users.noreply.github.com>
2021-08-09 16:31:39 +0530

    Add files via upload

---
+++ primeNumber.java
@@ -0,0 +1,39 @@
+package week1.day1;
+
+import java.util.Iterator;
+
+public class primeNumber {
+
+	public static void main(String[] args) {
+
+		int a=13;
+        boolean flag=false;
+        int b;
+
+for(int i=2;i<a/2; i++)			
+
+{
+	int m;
+	m=a%i;
+	if(m==0)
+	{
+		boolean flag1 =true;
+		break;
+
+	}
+}
+		
+if(flag==false)
+{
+	System.out.println("The Number is Prime");
+}
+else
+	
+{
+	System.out.println("The Number is not Prime");
+}
+
+
+}
+}
+
